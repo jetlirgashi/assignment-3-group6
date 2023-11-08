3e0b7f2be223fe976cd3f603d61163bd239797ad
Jetlir <jetlir2010@gmail.com>
2023-11-08 18:47:55 +0100

    Updated Route53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -6,7 +6,13 @@ resource "aws_route53_record" "www" {
   name    = "jetlir-assignment3.${data.aws_route53_zone.appstellar-training.name}"
   type    = "A"
   ttl     = 300
-  records = [data.aws_lb.this.dns_name]
+  
+  alias {
+    name                   = data.aws_lb.this.dns_name
+    zone_id                = data.aws_lb.this.zone_id
+    evaluate_target_health = true
+  }
+
 }
 data "aws_lb" "this" {
   tags = {
