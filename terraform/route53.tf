cb2880d2c41b2bbc65284c53fa5b3bc58c60928c
Jetlir <jetlir2010@gmail.com>
2023-11-08 18:41:17 +0100

    Updated Route53

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,20 +1,16 @@
 data "aws_route53_zone" "appstellar-training" {
   name = "appstellar.training"
 }
-
-
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.appstellar-training.zone_id
   name    = "jetlir-assignment3.${data.aws_route53_zone.appstellar-training.name}"
   type    = "A"
   ttl     = 300
-  records = ["8.8.8.8"]
+  records = [data.aws_lb.this.dns_name]
 }
-
 data "aws_lb" "this" {
-
   tags = {
-    Name       = "devops-training"
-    Enviroment = "dev"
+    Name        = "devops-training"
+    Environment = "dev"
   }
 }
